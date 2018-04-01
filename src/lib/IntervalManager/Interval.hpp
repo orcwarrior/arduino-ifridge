65b64f2fe3e6ac4bca7d1ad88cc344ec6a40ffdc
orcwarrior <orcwarrior@o2.pl>
2018-04-01 22:15:29 +0200

    Interval.hpp: fixed intervals firing always cause of u long int to int conversion

--- src/lib/IntervalManager/Interval.hpp
+++ src/lib/IntervalManager/Interval.hpp
@@ -13,13 +13,12 @@ public:
   {
     this->callback = cb;
     this->interval = interval;
-    this->once = once;
+    this->once     = once;
   }
 
-  bool tryRun(long millis)
+  bool tryRun(unsigned long millis)
   {
-    if (!this->nextRun || this->nextRun <= millis)
-    {
+    if (this->nextRun <= millis) {
       this->nextRun = millis + this->interval;
       this->callback(this);
       return true;
@@ -27,10 +26,12 @@ public:
     return false;
   }
 
-  void setInterval(int newInterval) {
-    const int intervalDiff = this->interval -  newInterval;
-    this->nextRun -= intervalDiff;
-    this->interval =  newInterval;
+  void setInterval(int newInterval)
+  {
+    if (newInterval <= 0) newInterval = 1;
+    const int intervalDiff = this->interval - newInterval;
+    if (this->nextRun - intervalDiff >= 0) this->nextRun -= intervalDiff;
+    this->interval = newInterval;
   }
 
   int getInterval() { return this->interval; }
@@ -38,11 +39,10 @@ public:
   bool isOneTime() { return this->once; }
 
 private:
-  int interval = 1000;
-  int nextRun = 0; // so it'll run interval cb at the beggining
-  bool once; // run interval only one time (handled by manager)
+  int interval          = 1000;
+  unsigned long nextRun = 0; // so it'll run interval cb at the beggining
+  bool once;                 // run interval only one time (handled by manager)
   IntervalCallback callback;
-  
 };
 
 #endif
